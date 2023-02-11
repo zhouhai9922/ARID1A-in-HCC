81f005b8868f794e317cdbc86ae76f9f01d77025
zhouhai9922 <124052689+zhouhai9922@users.noreply.github.com>
2023-02-11 10:39:26 +0800

    Add files via upload

---
+++ Nomo.R
@@ -0,0 +1,79 @@
+######Video source: https://ke.biowolf.cn
+######??????ѧ??: https://www.biowolf.cn/
+######΢?Ź??ںţ?biowolf_cn
+######???????䣺biowolf@foxmail.com
+######????΢??: 18520221056
+
+#install.packages("survival")
+#install.packages("regplot")
+#install.packages("rms")
+
+
+#???ð?
+library(survival)
+library(regplot)
+library(rms)
+
+riskFile="risk.TCGA.txt"     #?????ļ?
+cliFile="clinical.txt"       #?ٴ??????ļ?
+setwd("C:\\Users\\MOMO")     #???ù???Ŀ¼
+
+#??ȡ?????????ļ?
+risk=read.table(riskFile, header=T, sep="\t", check.names=F, row.names=1)
+
+#??ȡ?ٴ??????ļ?
+cli=read.table(cliFile, header=T, sep="\t", check.names=F, row.names=1)
+cli=cli[apply(cli,1,function(x)any(is.na(match('unknow',x)))),,drop=F]
+cli$Age=as.numeric(cli$Age)
+
+#?ϲ?????
+samSample=intersect(row.names(risk), row.names(cli))
+risk1=risk[samSample,,drop=F]
+cli=cli[samSample,,drop=F]
+rt=cbind(risk1[,c("futime", "fustat", "Risk")], cli)
+
+#????????ͼ
+res.cox=coxph(Surv(futime, fustat) ~ . , data = rt)
+nom1=regplot(res.cox,
+              plots = c("density", "boxes"),
+              clickable=F,
+              title="",
+              points=TRUE,
+              droplines=TRUE,
+              observation=rt[9,],
+              rank="sd",
+              failtime = c(1,3,5),
+              prfail = F)
+
+#????????ͼ?ķ??մ????ļ?
+nomoRisk=predict(res.cox, data=rt, type="risk")
+rt=cbind(risk1, Nomogram=nomoRisk)
+outTab=rbind(ID=colnames(rt), rt)
+write.table(outTab, file="nomoRisk.txt", sep="\t", col.names=F, quote=F)
+
+#У׼????
+pdf(file="calibration.pdf", width=5, height=5)
+#1??У׼????
+f <- cph(Surv(futime, fustat) ~ Nomogram, x=T, y=T, surv=T, data=rt, time.inc=1)
+cal <- calibrate(f, cmethod="KM", method="boot", u=1, m=(nrow(rt)/3), B=1000)
+plot(cal, xlim=c(0,1), ylim=c(0,1),
+	 xlab="Nomogram-predicted OS (%)", ylab="Observed OS (%)", lwd=1.5, col="green", sub=F)
+#3??У׼????
+f <- cph(Surv(futime, fustat) ~ Nomogram, x=T, y=T, surv=T, data=rt, time.inc=3)
+cal <- calibrate(f, cmethod="KM", method="boot", u=3, m=(nrow(rt)/3), B=1000)
+plot(cal, xlim=c(0,1), ylim=c(0,1), xlab="", ylab="", lwd=1.5, col="blue", sub=F, add=T)
+#5??У׼????
+f <- cph(Surv(futime, fustat) ~ Nomogram, x=T, y=T, surv=T, data=rt, time.inc=5)
+cal <- calibrate(f, cmethod="KM", method="boot", u=5, m=(nrow(rt)/3), B=1000)
+plot(cal, xlim=c(0,1), ylim=c(0,1), xlab="", ylab="",  lwd=1.5, col="red", sub=F, add=T)
+legend('bottomright', c('1-year', '3-year', '5-year'),
+	   col=c("green","blue","red"), lwd=1.5, bty = 'n')
+dev.off()
+
+
+######Video source: https://ke.biowolf.cn
+######??????ѧ??: https://www.biowolf.cn/
+######΢?Ź??ںţ?biowolf_cn
+######???????䣺biowolf@foxmail.com
+######????΢??: 18520221056
+
